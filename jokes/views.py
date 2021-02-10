046c5974c5b5e41b8569b11cfffba25e9960e357
Tiago Yamamoto <japa@rede5.com.br>
2021-02-10 14:54:55 -0300

    url e template

--- jokes/views.py
+++ jokes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+def index(request):
+    url = 'http://api.icndb.com/jokes/random/'
+    response = requests.get(url).json()
+    joke = response['value']['joke']
+    return render(request, 'index.html', context={'text': joke})
